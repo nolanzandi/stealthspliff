215595dd0283564a5fc5e787b0d1a59c96b8594b
Nolan Zandi <nolan@starkenburg.com>
2013-04-02 17:45:22 -0700

    config changes

--- blog/wp-config.php
+++ blog/wp-config.php
@@ -22,10 +22,10 @@ define('DB_NAME', 'wordpress');
 define('DB_USER', 'root');
 
 /** MySQL database password */
-define('DB_PASSWORD', '');
+define('DB_PASSWORD', 'Albooboo420');
 
 /** MySQL hostname */
-define('DB_HOST', 'localhost');
+define('DB_HOST', 'wordpress.cduec6bqotn4.us-west-1.rds.amazonaws.com');
 
 /** Database Charset to use in creating database tables. */
 define('DB_CHARSET', 'utf8');
